7b98991c5962b455ef0bca89966a4bf413de8071
valdot <45897344+valdot@users.noreply.github.com>
2019-01-05 15:00:55 +0000

    Create call.php

---
+++ call.php
@@ -0,0 +1,23 @@
+<?php
+// Limit 3x Telpon Setiap Satu Nomor
+function send($phone){
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, "https://www.tokocash.com/oauth/otp");                      curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HEADER, true);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, "msisdn=$phone&accept=call");                        $asw = curl_exec($ch);
+        curl_close($ch);
+                echo $asw."\n";
+}
+echo "[+]COPYRIGHT ; MR.TVM4\n\n";
+echo "[+]SOSMED:\n\n";
+echo "[+]FACEBOOK: ImanuelTaroreh\n\n";
+echo "[+]INSTAGRAM: valdotaroreh19\n\n";
+echo "[+]GITHUB: http://github.com/valdot\n\n";
+echo "Nomor\Input : ";
+$nomor = trim(fgets(STDIN));
+$execute = send($nomor);
+print $execute;
+?>
